93286959b27b3c1faf8a075c9bd749847507c769
Simon Petersson <Peterssonaren@gmail.com>
2016-05-14 10:35:24 +0200

    Globally inject lodash

--- src/app/app.js
+++ src/app/app.js
@@ -1,18 +1,20 @@
-angular.module( 'fnfjs', [
+angular.module('fnfjs', [
   'templates-app',
   'templates-common',
   'fnfjs.home',
   'ui.router'
 ])
 
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+.constant('_', window._)
+
+.config(function myAppConfig ( $stateProvider, $urlRouterProvider ) {
   $urlRouterProvider.otherwise( '/home' );
 })
 
-.run( function run () {
+.run(function run () {
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+.controller('AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + ' | fnfjs' ;
